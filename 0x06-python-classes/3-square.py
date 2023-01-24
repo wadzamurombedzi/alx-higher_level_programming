a695c1b1ffff4cdd82fd6e1d90949f5bf7c22b2f
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2023-01-24 13:34:44 -0800

    Square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
@@ -20,4 +19,3 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-

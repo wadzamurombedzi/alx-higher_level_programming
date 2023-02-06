213f7bdede0ec09ec783c8e872f5cbf81f66793c
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2023-02-06 14:44:17 -0800

    My int

---
+++ 0x0A-python-inheritance/100-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+MyInt = __import__('100-my_int').MyInt
+
+my_i = MyInt(3)
+print(my_i)
+print(my_i == 3)
+print(my_i != 3)
+
+guillaume@ubuntu:~/0x0A$ ./100-main.py
+3
+False
+True
